f1d2adb24a84fe3f792adf83210e8b0e20ec908a
bailey everts <texas312@gmail.com>
2022-12-18 00:21:38 -0700

    chore: scaffolding/semantic-release [#1]

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.0.0"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="discord-sls",
+    version=__version__,
+    license="MIT",
+    author="Bailey Everts",
+    author_email="bailey@evertsenterprises.com",
+    description="For building serverless discord bots",
+    url="https://github.com/beverts312/discord-sls",
+    packages=find_packages(),
+    install_requires=["requests", "PyNaCl"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
---
+++ tasks.py
@@ -0,0 +1,35 @@
+from invoke import task
+
+
+def black(c, check):
+    c.run("isort .")
+    return c.run(
+        f"black tasks.py discord_sls/ --line-length=79 {'--check' if check is True else ''} -v"
+    )
+
+
+@task(aliases=["f"])
+def format(c):
+    return black(c, False)
+
+
+@task(aliases=["cf", "fc"])
+def check_format(c):
+    return black(c, True)
+
+
+@task(aliases=["c"])
+def clean(c):
+    return c.run("rm -rf dist discord_sls.egg-info build")
+
+
+@task(aliases=["b"])
+def build(c):
+    return c.run("python3 setup.py bdist_wheel")
+
+
+@task(aliases=["bp"])
+def build_and_publish(c):
+    clean(c)
+    build(c)
+    return c.run("python3 -m twine upload dist/*.whl --verbose")
